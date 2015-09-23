64a477ac22c1001860f2f2a61531d5fd25dabcba
kokosko <kostya.kokosko@gmail.com>
2015-09-23 10:03:48 +0300

    migrate to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150922141746) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "text"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20150922141746) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
@@ -32,4 +35,5 @@ ActiveRecord::Schema.define(version: 20150922141746) do
     t.string   "image"
   end
 
+  add_foreign_key "messages", "users"
 end
