1c2e5e7cdc6bd6df2c202403c8f6128e9123039d
kokosko <kostya.kokosko@gmail.com>
2015-09-24 14:59:50 +0300

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,13 +14,13 @@ Rails.application.configure do
 
   config.assets.digest = true
 
-  config.log_level = :debug
+  # config.log_level = :debug
 
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
 
-  config.log_formatter = ::Logger::Formatter.new
+  # config.log_formatter = ::Logger::Formatter.new
 
   config.active_record.dump_schema_after_migration = false
 
